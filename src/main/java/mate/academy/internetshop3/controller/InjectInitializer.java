b57844e5db63f84d97e1ab036f608ba71cdb91ed
lenovo <suleimanovayev@gmail.com>
2019-09-24 01:00:05 +0300

    added solution internet-shop log

--- src/main/java/mate/academy/internetshop3/controller/InjectInitializer.java
+++ src/main/java/mate/academy/internetshop3/controller/InjectInitializer.java
@@ -2,14 +2,20 @@ package mate.academy.internetshop3.controller;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+
 import mate.academy.internetshop3.lib.Injector;
+import org.apache.log4j.Logger;
 
 public class InjectInitializer implements ServletContextListener {
+    private static final Logger logger = Logger.getLogger(InjectInitializer.class);
+
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         try {
+            logger.info("Dependency injection started...");
             Injector.injectDependency();
         } catch (IllegalAccessException e) {
+            logger.fatal("Inject has failed" + e);
             throw new RuntimeException(e);
         }
     }
